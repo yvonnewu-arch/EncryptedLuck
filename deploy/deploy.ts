19af0fbd6b9196a33468d30128420bababa6b1ac
yvonnewu-arch <caabbkei73469@outlook.com>
2025-12-24 10:47:55 +0800

    Wire deploy script for EncryptedLuck

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployed = await deploy("EncryptedLuck", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`EncryptedLuck contract: `, deployed.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_encryptedLuck";
+func.tags = ["EncryptedLuck"];
